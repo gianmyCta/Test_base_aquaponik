d9737db91ab8a69f1d7b13cd904ac173728d1060
Gianmarco <gianmy_martino@hotmail.it>
2026-05-19 11:59:53 +0200

    chore

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@
 set PROJECT_ROOT=%~dp0
 
 :: FRONTEND
-start cmd /k "cd /d %PROJECT_ROOT%Frontend && npm run dev"
+start cmd /k "cd /d %PROJECT_ROOT%Frontend && npm run dev -- --host"
 
 :: BACKEND
 start cmd /k "cd /d %PROJECT_ROOT%Backend && venv\Scripts\activate.bat && python -m uvicorn app.main:app --host 127.0.0.1 --port 8001"
